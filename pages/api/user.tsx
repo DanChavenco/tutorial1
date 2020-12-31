3681d7810b67b62961ed22d922cec253c00641b5
DanChavenco <daniel.dcn@hotmail.com>
2020-12-31 11:00:57 -0300

    API routes: teacher, search, appointments

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -6,11 +6,17 @@ interface ErrorResponseType {
 };
 
 interface SuccessResponseType {
-  _id: string;
-  name: string;
-  email: string;
-  cellphone: string;
-  teacher: boolean;
+  _id: string,
+  name: string,
+  email: string,
+  cellphone: string,
+  teacher: boolean,
+  coins: Int16Array,
+  courses: string[],
+  available_hours: object[],
+  available_locations: string[],
+  appoitments: object[],
+  reviews: object[]
 }
 
 export default async(
@@ -18,10 +24,18 @@ export default async(
   res: NextApiResponse<ErrorResponseType | SuccessResponseType>
   ): Promise<void> => {
     if (req.method === "POST") {
-      const { name, email, cellphone, teacher } = req.body;
-      if (!name || !email || !cellphone || !teacher) {
-        res.status(400).json({error: "Missing body parameter."});
-        return;
+      const { name, email, cellphone, teacher, courses, available_hours, available_locations } = req.body;
+
+      if (!teacher) {
+        if (!name || !email || !cellphone) {
+          res.status(400).json({error: "Missing body parameter."});
+          return;
+        }
+      } else if (teacher) {
+        if (!name || !email || !cellphone || !courses || !available_hours || !available_locations) {
+          res.status(400).json({error: "Missing body parameter."});
+          return;
+        }
       }
 
       const { db } = await connect();
@@ -30,10 +44,33 @@ export default async(
         name, 
         email, 
         cellphone, 
-        teacher
+        teacher,
+        coins: 1,
+        courses: courses || [],
+        available_hours: available_hours || {},
+        available_locations: available_locations || [],
+        appoitments: [],
+        reviews: []
       });
 
       res.status(200).json(response.ops[0]);
+    } else if (req.method === "GET") {
+      const { email } = req.body;
+
+      if (!email) {
+        res.status(400).json({error: "Missing e-mail on request body."});
+        return;
+      }
+
+      const { db } = await connect();
+
+      const response = await db.collection('users').findOne({ email });
+
+      if (!response) {
+        res.status(400).json({error: "User with this e-mail not found."});
+        return;
+      }
+      res.status(200).json(response);
     } else {
       res.status(400).json({error: "Wrong request method."})
     }
